052f07e1e57948382640897fd1cf24f67f6a8e97
Al Randolph Villegas <100111521+VILLEGASAL@users.noreply.github.com>
2025-03-16 22:45:29 -0700

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 import styles from "../styles/login.module.css";
 import { useQuery } from "@tanstack/react-query";
 
-export const baseUrl = `https://attendance-backend-9ilt.onrender.com`;
+export const baseUrl = `https://attendance-backend-1-xwk9.onrender.com`;
 
 export const Login = () => {
 
